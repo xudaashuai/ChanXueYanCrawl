77713422443fdca21a0407b674d51de4c54f59ef
xudashuai <xudashuai0819@outlook.com>
2017-11-10 23:02:32 +0800

    修改

--- test_rule.py
+++ test_rule.py
@@ -1,8 +1,5 @@
 from xtp import *
-import pymongo,re
-connection = pymongo.MongoClient('101.236.6.203', 27017)
-tdb = connection.data
-comments = tdb.comments
+import re
 ms = [[
     {
         'POSTAG': 'n.?',
@@ -35,19 +32,8 @@ ms = [[
     }
 ]]
 
-test_num = 10000
-page = 10
-num = 0
 
 s = '([^ ^#]+) n.? \d+ 主谓关系(?:#([^ ]+) d.? \d+ [^#]+)*#([^ ]+) a.? [^ ]+ [^#]+'
 r = re.compile(s)
 
-
-for i in range(test_num//page):
-    for k,item in enumerate(comments.find({'hanlp_result':{'$ne':None}}).limit(page).skip(i * page)):
-        hanlp_result = item['hanlp_result']
-        s = Sentence(hanlp_result)
-        result = r.findall(s.str())
-        num += len(result)!=0
-        print(num,'/',i*page+k,'/',10000,)
-        print(s.clear_str(),'\n',*result,*s.match(ms))
+test_by_dict(1000,ms,False)
