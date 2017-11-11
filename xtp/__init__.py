abf5e8ce85d34f4372efc0b990901bb52bfe880d
xudashuai <xudashuai0819@outlook.com>
2017-11-12 00:49:46 +0800

    修改

--- xtp/__init__.py
+++ xtp/__init__.py
@@ -6,22 +6,18 @@ connection = pymongo.MongoClient('101.236.6.203', 27017)
 tdb = connection.data
 comments = tdb.comments
 
-f = open('1.txt','w+',encoding='utf-8')
-def test_by_dict(test_num, rule,debug = True):
+def test_by_dict(test_num, rule, debug=True):
     page = 1000
     num = 0
     for i in range(test_num // page):
         for k, item in enumerate(comments.find({'hanlp_result': {'$ne': None}}).limit(page).skip(i * page)):
             hanlp_result = item['hanlp_result']
-            f.write(hanlp_result)
-            f.write('\n')
             s = Sentence(hanlp_result)
             result = s.match(rule)
             num += len(result) != 0
             if debug:
-                print(num, '/', i * page + k+1, '/', test_num)
-                print('\t',s.clear_str())
-                print('\t',*result)
+                print(num, '/', i * page + k + 1, '/', test_num)
+                print('\t', s.clear_str())
+                print('\t', *result)
             else:
-                print(num, '/', i * page + k+1, '/', test_num)
-    f.close()
+                print(num, '/', i * page + k + 1, '/', test_num)
