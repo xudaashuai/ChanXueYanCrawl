815879f3d9cfdd3538bc6e917e798442c08d0058
徐大帅 <xudashuai0819@outlook.com>
2017-11-17 00:20:51 +0800

    修改

--- test_rule.py
+++ test_rule.py
@@ -1,9 +1,19 @@
 from xtp import *
-import pymongo, re,json
+
 
 def rule1(sentence):
     for word in sentence.words:
+        adv = ''
         if word.DEPREL == '主谓关系':
-            yield (word.FORM,sentence.words[int(word.HEAD)-1].FORM)
+            adj = sentence.words[int(word.HEAD) - 1]
+            if 'a' in adj.CPOSTAG:
+                for word2 in sentence.words:
+                    if word2.HEAD == adj.ID and word2.DEPREL == '状中结构' and 'd' in word2.CPOSTAG:
+                        adv = word2.FORM
+                yield (word.FORM, adv, adj.FORM)
 
-test_by_rule(1000,[rule1])
+
+with open('res.txt','w+',encoding='utf-8') as f:
+    for item in test_by_rule(1000, [rule1]):
+        f.write(str('-'.join(item)))
+        f.write('\n')
--- xtp/op.py
+++ xtp/op.py
@@ -11,15 +11,19 @@ def test_by_rule(test_num, rule, debug=True):
     page = 1000
     num = [0, 0, 0, test_num]
     for i in range(test_num // page):
+        print(i,num)
         for k, item in enumerate(comments.find({'hanlp_result': {'$ne': None}}).limit(page).skip(i * page)):
             hanlp_result = item['hanlp_result']
+            t = item
             s = Sentence(hanlp_result)
             item = s.match_test(rule)
             num[0]+=1
             for it in item[1]:
                 num[-2]+=len(it[1])
             num[-3]+=len(item[1])!=0
-            print(num)
-            print('\t',item[0])
+            # print(num)
+            # print('\t',item[0])
             for item in item[1]:
-                print('\t',item)
+                # print('\t',item)
+                for item in item[1]:
+                    yield (*item,str(t['score']))
