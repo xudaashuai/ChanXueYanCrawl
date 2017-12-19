a268c84b45ddd2c040fcdd54dc60ce30c7784171
徐大帅 <xudashuai0819@outlook.com>
2017-12-19 14:41:27 +0800

    修改

--- BDSpider.py
+++ BDSpider.py
@@ -12,27 +12,28 @@ SECRET_KEY = 'at8ObT3K6ruzgB4tg2TIz9Tb8visZkY8'
 
 client = AipNlp(APP_ID, API_KEY, SECRET_KEY)
 api_url = 'https://aip.baidubce.com/rpc/2.0/nlp/v2/comment_tag'
-
-comments = tdb.meituan_comment.find({'baidu_result': None})
+t = 0
 d = {'success': 0, 'error': 0}
-for i,item in enumerate(comments):
-    try:
-        it = client.commentTag(item['text'])
-    except Exception as e:
-        d['error']+=1
-        tdb.meituan_comment.update(item, {"$set": {"baidu_result": 'error'}})
-        continue
+while True:
+    t +=1
+    for i,item in enumerate(tdb.meituan_comment.find({'baidu_result': None}).skip(t*1000).limit(1000)):
+        try:
+            it = client.commentTag(item['text'])
+        except Exception as e:
+            d['error']+=1
+            tdb.meituan_comment.update(item, {"$set": {"baidu_result": 'error'}})
+            continue
 
-    if 'error_code' in it:
-        if it['error_code'] == 19:
-            exit(0)
-        if it['error_msg'] not in d:
-            d[it['error_msg']]=0
-        d[it['error_msg']]+=1
-    else:
-        d['success']+=1
-    if i%100==0:
-        print(d)
+        if 'error_code' in it:
+            if it['error_code'] == 19:
+                exit(0)
+            if it['error_msg'] not in d:
+                d[it['error_msg']]=0
+            d[it['error_msg']]+=1
+        else:
+            d['success']+=1
+        if i%100==0:
+            print(d)
 
-    tdb.meituan_comment.update(item, {"$set": {"baidu_result": [(ite['prop'],ite['adj']) for ite in it.get('items',[])]}})
+        tdb.meituan_comment.update(item, {"$set": {"baidu_result": [(ite['prop'],ite['adj']) for ite in it.get('items',[])]}})
 
